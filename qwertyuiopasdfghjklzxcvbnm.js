c0beaa504c56478c29ef0f366e90abb62b702979
ZUUNOTFOUND <zuunotf@gmail.com>
2024-12-17 15:02:11 +0900

    Update qwertyuiopasdfghjklzxcvbnm.js

--- qwertyuiopasdfghjklzxcvbnm.js
+++ qwertyuiopasdfghjklzxcvbnm.js
@@ -2,5 +2,6 @@
   "6283890875133@s.whatsapp.net",
   "6283840095883@s.whatsapp.net",
   "6287872921691@s.whatsapp.net",
-  "6283173488914@s.whatsapp.net"
+  "6283173488914@s.whatsapp.net",
+  "6287779469491@s.whatsapp.net"
 ]
